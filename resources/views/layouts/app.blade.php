791e64414aae60df6ac452c0a46f4e71bc97b4f4
Bo Bo <bobo54157.dev@gmail.com>
2021-04-19 20:24:48 +0630

    products load - ajax

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name') }}</title>
 
@@ -47,7 +48,7 @@
     <script>
         let locale = '{{ session('locale') }}';
     </script>
-    
+
     @yield('script')
 </body>
 </html>
