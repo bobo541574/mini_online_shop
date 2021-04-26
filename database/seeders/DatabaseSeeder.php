da2dca234f617e2a2f56b10128a6fff5dba5bbcf
Bo Bo <bobo54157.dev@gmail.com>
2021-04-26 18:01:48 +0630

    permission & role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,33 +18,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::create([
-        //     'role_id' => 1,
-        //     'first_name' => "Super",
-        //     'last_name' => "Admin",
-        //     'user_name' => "super admin",
-        //     'email' => "superadmin@gmail.com",
-        //     'email_verified_at' => now(),
-        //     'phone' => "09999999999",
-        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        //     'remember_token' => Str::random(10),
-        // ]);
+        User::create([
+            'role_id' => 1,
+            'first_name' => "Super",
+            'last_name' => "Admin",
+            'user_name' => "super admin",
+            'email' => "superadmin@gmail.com",
+            'email_verified_at' => now(),
+            'phone' => "09999999999",
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
 
-        // PermissionRole::truncate();
+        PermissionRole::truncate();
 
         $this->call([
-            // RoleSeeder::class,
-            // UserSeeder::class,
+            RoleSeeder::class,
+            UserSeeder::class,
             PermissionSeeder::class,
             PermissionAssignSeeder::class,
-            // CategorySeeder::class,
-            // SubCategorySeeder::class,
-            // BrandSeeder::class,
-            // BrandCategorySeeder::class,
-            // ColorSeeder::class,
-            // SizeSeeder::class,
-            // ProductSeeder::class,
-            // ProductAttributeSeeder::class,
+            CategorySeeder::class,
+            SubCategorySeeder::class,
+            BrandSeeder::class,
+            BrandCategorySeeder::class,
+            ColorSeeder::class,
+            SizeSeeder::class,
+            ProductSeeder::class,
+            ProductAttributeSeeder::class,
         ]);
     }
 }
