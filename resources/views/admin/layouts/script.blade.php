54d95e1b09eb503ccba9ba9ec57694426bc88e71
Bo Bo <bobo54157.dev@gmail.com>
2021-08-15 00:36:06 +0630

    product local update

--- resources/views/admin/layouts/script.blade.php
+++ resources/views/admin/layouts/script.blade.php
@@ -2,4 +2,10 @@
 <script type="text/javascript" src="{{ asset('assets/jquery/jquery-3.2.1.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/select2/select2.min.js') }}"></script>
 
+<script>
+
+    const locale = "{{ session('locale') }}";
+
+</script>
+
 @yield('script')
