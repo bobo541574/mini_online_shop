cd0c08b26576232432d7fe6f24afd719de78aa11
Bo Bo <bobo54157.dev@gmail.com>
2021-08-15 00:38:51 +0630

    product local update

---
+++ app/Http/Requests/Back/Product/CreateRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests\Back\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize():bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules():array
+    {
+        return [
+            'name_en' => 'required',
+            'name_mm' => 'required',
+            'supplier' => 'required',
+            'category' => 'required',
+            'subcategory' => 'required',
+            'brand' => 'required',
+        ];
+    }
+
+    /**
+     * Set the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages():array
+    {
+            return [
+                'name_en.required' => 'product_name_en_required',
+                'name_mm.required' => 'product_name_mm_required',
+                'supplier.required' => 'supplier_required',
+                'category.required' => 'category_required',
+                'subcategory.required' => 'subcategory_required',
+                'brand.required' => 'brand_required',
+            ];
+    }
+}
